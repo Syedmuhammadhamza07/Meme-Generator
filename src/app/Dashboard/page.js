40a23f2137d8d35140e252b6fcaa1319173e8ec3
Syed Muhammad Hamza <syedmuhammadhamza2019@gmail.com>
2024-02-19 03:30:57 +0500

    initial code

--- src/app/Dashboard/page.js
+++ src/app/Dashboard/page.js
@@ -6,17 +6,22 @@ export default async function Dashboard() {
   const images = data.data.memes;
 
   return (
+    <div className="w-full m-2 bg-gradient-to-tl from-black to-slate-500 ">
+      <div className="flex items-center justify-center w-full my-0.5">
+          <h1 className="text-blue-950 font-semibold text-6xl">Dashboard</h1>
+      </div>
       <div className="main-card-div" > 
         {images.map((item, index) => (
           <div key={index}  className="mb-0 text-center lg:mb-0 lg:text-left">
             <Link href={`/detail/${item.id}`} passHref>
               <div className="border-2 p-2 w-60 h-auto m-3" rel="noopener noreferrer">
                 <img src={item.url} alt={item.name} width={100} height={100} className="p-2 w-52 h-52 " />
-                <span className="image-name">{item.name}</span>
+                <span className="image-name text-white text-center">{item.name}</span>
               </div>
             </Link>
           </div>
         ))}
       </div>
+    </div>
   );
 }
--- src/app/detail/[id]/page.js
+++ src/app/detail/[id]/page.js
@@ -1,51 +1,88 @@
-'use client'
+"use client";
 // import Detail from "./Detail";
 import Link from "next/link";
-import { useEffect, useState } from 'react';
+import { useEffect, useState } from "react";
 
+export default function Details({ params }) {
+  const [selectedMemeUrl, setSelectedMemeUrl] = useState("");
+  const [text1, setText1] = useState("");
+  const [text2, setText2] = useState("");
+  const [generatedMemeUrl, setGeneratedMemeUrl] = useState("");
 
-export default  function Details({params}){
-    const [selectedMemeUrl, setSelectedMemeUrl] = useState('');
-    const [text1, setText1] = useState('');
-    const [text2, setText2] = useState('');
-    const [generatedMemeUrl, setGeneratedMemeUrl] = useState('');
+  useEffect(() => {
+    fetchMeme();
+  }, []);
 
-    useEffect(() => {
-        fetchMeme();
-    }, []);
+  async function fetchMeme() {
+    const res = await fetch("https://api.imgflip.com/get_memes");
+    const result = await res.json();
+    const memes = result.data.memes.find((memes) => memes.id === params.id);
+    setSelectedMemeUrl(memes.url);
+  }
 
-    async function fetchMeme() {
-        const res = await fetch('https://api.imgflip.com/get_memes')
-        const result = await res.json()
-        const memes = result.data.memes.find(memes => memes.id === params.id);
-        setSelectedMemeUrl(memes.url)
+  async function generateMeme() {
+    const res = await fetch(
+      ` https://api.imgflip.com/caption_image?template_id=${params.id}&username=MuneebAhmed3&password=muneeb12&text0=${text1}&text1=${text2}`
+    );
+    const result = await res.json();
+    if (result.success) {
+      setGeneratedMemeUrl(result.data.url);
+    } else {
+      alert(result.error_message);
     }
+  }
 
-    async function generateMeme(){
-        const res = await fetch(` https://api.imgflip.com/caption_image?template_id=${params.id}&username=MuneebAhmed3&password=muneeb12&text0=${text1}&text1=${text2}`)
-        const result = await res.json()
-        if (result.success) {
-            setGeneratedMemeUrl(result.data.url);
-        } else {
-            alert(result.error_message);
-        }
-    } 
-
-
-
-    return <div> 
-         <img className='w-64 h-64 m-4' src={selectedMemeUrl} alt="Selected Meme" />
-         <div className='mb-4'>
-                <label htmlFor="text1" className='mr-2'>Text 1:</label>
-                <input className='h-12 p-2 border border-black rounded' type="text" id="text1" value={text1} onChange={(e) => setText1(e.target.value)} />
-            </div>
-            <div className='mb-4'>
-                <label htmlFor="text2" className='mr-2'>Text 2:</label>
-                <input className='h-12 p-2 border border-black rounded' type="text" id="text2" value={text2} onChange={(e) => setText2(e.target.value)} />
-            </div>
-            <button class="bg-blue-500 hover:bg-blue-400 text-white font-bold py-2 px-4 border-b-4 border-blue-700 hover:border-blue-500 rounded" onClick={generateMeme}>Generate</button>
-            <div>
-                 {generatedMemeUrl && <img className='w-64 h-64' src={generatedMemeUrl} alt="Generated Meme" />}
-            </div>
-    </div>
-    }
+  return (
+    <div className="w-full bg-gradient-to-tl from-black to-slate-600">
+        <div className="flex items-center justify-center w-full py-4">
+            <h1 className="text-white font-semibold text-2xl">Meme Generator</h1>
+        </div>
+      <div className="flex items-center justify-center w-full ">
+      <img
+        className="w-64 h-64 m-4"
+        src={selectedMemeUrl}
+        alt="Selected Meme"
+      />
+      <div className="mb-4">
+        <label htmlFor="text1" className="mr-2 text-lg font-semibold text-white">
+          Text 1:
+        </label>
+        <input
+          className="h-12 p-2 border bg-transparent border-white rounded"
+          type="text"
+          id="text1"
+          value={text1}
+          onChange={(e) => setText1(e.target.value)}
+        />
+      </div>
+      <div className="mb-4">
+        <label htmlFor="text2" className="mr-2 text-lg font-semibold text-white">
+          Text 2:
+        </label>
+        <input
+          className="h-12 p-2 border bg-transparent border-white rounded"
+          type="text"
+          id="text2"
+          value={text2}
+          onChange={(e) => setText2(e.target.value)}
+        />
+      </div>
+      <button
+        class="bg-blue-500 hover:bg-blue-400 text-white font-bold py-2 px-4 border-blue-700 hover:border-blue-500 rounded"
+        onClick={generateMeme}
+      >
+        Generate
+      </button>
+      </div>
+      <div>
+        {generatedMemeUrl && (
+          <img
+            className="w-72 h-64 p-4"
+            src={generatedMemeUrl}
+            alt="Generated Meme"
+          />
+        )}
+      </div>
+    </div>
+  );
+}
