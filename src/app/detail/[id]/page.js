d0d52ec1710331554f39997d73a14a803c474e91
Syed Muhammad Hamza <syedmuhammadhamza2019@gmail.com>
2024-03-01 02:32:28 +0500

    initial code

--- src/app/detail/[id]/page.js
+++ src/app/detail/[id]/page.js
@@ -1,5 +1,9 @@
 "use client";
 import { useEffect, useState } from "react";
+import Image from "next/image";
+import Link from "next/link";
+
+import webLogo from "./logo.png";
 
 export default function Details({ params }) {
   const [selectedMemeUrl, setSelectedMemeUrl] = useState("");
@@ -31,63 +35,83 @@ export default function Details({ params }) {
   }
 
   return (
-    <div className="w-full h-screen bg-gradient-to-tl from-black to-slate-600">
-      <div className="border-blue-50 border w-fit m-auto">
-      <div className="flex items-center justify-center w-full py-4">
-        <h1 className="text-white font-semibold text-2xl">Meme Generator</h1>
-      </div>
-      <div className="flex items-center justify-center w-full">
-        <img
-          className="w-64 h-64 m-4"
-          src={selectedMemeUrl}
-          alt="Selected Meme"
-        />
-        <div className="mb-4 flex flex-col mx-2">
-          <label
-            htmlFor="text1"
-            className="mr-2 text-lg font-semibold text-white"
-          >
-            Text 1:
-          </label>
-          <input
-            className="h-12 p-2 border bg-transparent text-white border-white rounded"
-            type="text"
-            id="text1"
-            value={text1}
-            onChange={(e) => setText1(e.target.value)}
-          />
+    <div>
+      <div className="w-full shadow-sm shadow-slate-600 flex items-center justify-between">
+      <Link href={"/"} passHref>
+        <Image src={webLogo} className="w-32 mr-2" />
+      </Link>
+        <div className="flex items-center w-full sm:w-auto">
+          <p className="text-base text-red-600 font-medium mx-2">HOME</p>
         </div>
-        <div className="mb-4 flex flex-col">
-          <label
-            htmlFor="text2"
-            className="mr-2 text-lg font-semibold text-white"
-          >
-            Text 2:
-          </label>
-          <input
-            className="h-12 p-2 border bg-transparent text-white border-white rounded"
-            type="text"
-            id="text2"
-            value={text2}
-            onChange={(e) => setText2(e.target.value)}
-          />
-        </div>
-          <button
-            class="bg-red-500 hover:bg-red-400 text-white font-bold p-2 h-12 border-red-700 hover:border-red-500 rounded mx-2 mt-2.5"
-            onClick={generateMeme}
-          >
-            Generate
+        <div className="flex items-center justify-end w-full">
+          <button className="text-base text-red-600 bg-transparent font-medium mx-2">
+            Login
           </button>
+          <button className="text-base text-white bg-red-600 p-1 rounded-md font-medium mx-2">
+            Sign Up
+          </button>
+        </div>
       </div>
-      </div>
-      <div>
-        {generatedMemeUrl && (
-          <img
-            className="w-72 h-64 p-4"
-            src={generatedMemeUrl}
-            alt="Generated Meme"
-          />
-        )}
+      <div className="w-full m-auto mt-5 flex flex-col items-center justify-center">
+        <div className="border-gray-500 border w-full h-fit flex flex-col items-center justify-center">
+          <div className="flex items-center justify-center w-full py-4">
+            <h1 className="text-gray-600 font-semibold text-2xl">
+              Meme Generator
+            </h1>
+          </div>
+          <div className="flex items-center justify-center w-full sm:flex-col">
+            <img
+              className="w-64 h-64 m-4"
+              src={selectedMemeUrl}
+              alt="Selected Meme"
+            />
+            <div className="mb-4 flex flex-col mx-2">
+              <label
+                htmlFor="text1"
+                className="mr-2 text-lg font-semibold text-gray-600"
+              >
+                Text 1:
+              </label>
+              <input
+                className="h-12 p-2 border bg-transparent text-gray-600 border-gray-800 rounded"
+                type="text"
+                id="text1"
+                value={text1}
+                onChange={(e) => setText1(e.target.value)}
+              />
+            </div>
+            <div className="mb-4 flex flex-col">
+              <label
+                htmlFor="text2"
+                className="mr-2 text-lg font-semibold text-gray-600"
+              >
+                Text 2:
+              </label>
+              <input
+                className="h-12 w-auto p-2 border bg-transparent text-gray-600 border-gray-800 rounded"
+                type="text"
+                id="text2"
+                value={text2}
+                onChange={(e) => setText2(e.target.value)}
+              />
+            </div>
+            <button
+              className="bg-red-500 hover:bg-red-400 text-white font-bold p-2 h-12 border-red-700 hover:border-red-500 rounded  m-2.5"
+              onClick={generateMeme}
+            >
+              Generate
+            </button>
+          </div>
+        </div>
+        <div className="sm:mt-3">
+          {generatedMemeUrl && (
+            <img
+              className="w-72 h-64 p-4"
+              src={generatedMemeUrl}
+              alt="Generated Meme"
+            />
+          )}
+        </div>
       </div>
     </div>
   );
--- src/app/page.js
+++ src/app/page.js
@@ -1,42 +1,50 @@
 import Image from "next/image";
 import Link from 'next/link'
 
+import Dashboard from "./Dashboard/page";
+
+import logo from "./images/logo.png"
+
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.js</code>
-        </p>
-      </div>
+    // <main className="flex min-h-screen flex-col items-center justify-between">
+    //   <div className="text-center lg:max-w-5xl lg:w-full h-screen lg:mb-0 lg:text-left flex items-center justify-center">
+    //     <Link
+    //      href="/Dashboard"
+    //       className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
+    //       rel="noopener noreferrer"
+    //     >
+    //       <h2 className={`mb-3 text-2xl font-semibold`}>
+    //       Dashboard{" "}
+    //         <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
+    //           -&gt;
+    //         </span>
+    //       </h2>
+    //     </Link>        
+    //   </div>
+    // </main>
+      <div className="flex min-h-screen flex-col items-center justify-between">
+        <div className="w-screen shadow-sm shadow-slate-600 flex items-center justify-between">
+          <Image src={logo} className="w-16 mr-2"/>
+          <div className="flex items-center w-full">
+          <p className="text-base text-red-600 font-medium mx-2">HOME</p>
+          <p className="text-base text-red-600 font-medium mx-2">Meme Generator</p>
+          </div>
+          <div className="flex items-center justify-end w-full">
+          <button className="text-base text-red-600 bg-transparent font-medium mx-2">Login</button>
+          <button className="text-base text-white bg-red-600 p-1 rounded-md font-medium mx-2">Sign Up</button>
+          </div>
+        </div>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+        <div className="w-screen mt-2">
+          <div className="flex flex-col items-center w-full">
+          <h3 className="text-base text-gray-700 font-medium mx-2">Meme generator</h3>
+          <p className="text-base text-gray-700 font-medium mx-2">Create a meme from <strong>JPG</strong>, <strong>GIF</strong> or <strong>PNG</strong> images.
+Edit your image and make a meme.</p>
+          </div>
+        <Dashboard />
+        </div>
 
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-       
-        <Link
-         href="/Dashboard"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-          Dashboard{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-        </Link>        
       </div>
-    </main>
   );
   }
